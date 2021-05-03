8a5a65db086f118943dce2ddd3b8f8c598d29132
Taylor <ivingstaylor@gmail.com>
2021-05-02 23:11:05 -0700

    new button logos, bug: can't run on phone

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, TouchableOpacity, Touchable } from 'react-native';
+import logo from './assets/tree_logo.png'
+import { Icon, InlineIcon } from '@iconify/react';
+import carrotIcon from '@iconify/icons-dashicons/carrot';
+import fruitWatermelon from '@iconify/icons-mdi/fruit-watermelon';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={logo} style={{width: 150, height: 241}} />
+
+      <View style={styles.buttons}>
+        <TouchableOpacity>
+          <Icon icon={fruitWatermelon} style={{color: '#ffffff', fontSize: '40px'}} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Icon icon={carrotIcon} style={{color: '#ffffff', fontSize: '40px'}} />
+        </TouchableOpacity>
+        </View>
     </View>
+
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#93E7C9',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttons: {
+    width: 240,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 20
+  }
 });
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ripen
+
+Ripen is a shopping companion app that allows users to learn more about the fruits and vegetables they plan on ingesting.
